a0ccdfb6166a98a69c7d66d96504320bd00d9c51
Stryfura <shilunett@gmail.com>
2024-08-03 18:07:10 +0900

    Added implementation of basic read all, read 1, create and delete operations (+ basic sqlite3 db structure). Added sqlite file too for reference

--- app.py
+++ app.py
@@ -1,10 +1,79 @@
-from flask import Flask
+import sqlite3
+
+from flask import Flask, request, jsonify
+import json
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Sup'
+
+def db_connection():
+    conn = None
+    try:
+        conn = sqlite3.connect('invoices.sqlite')
+    except sqlite3.Error as e:
+        print(e)
+    return conn, conn.cursor()
+
+
+@app.route("/invoices", methods=["GET", "POST"])
+def invoices():
+    conn, cursor = db_connection()
+
+    if request.method == "GET":
+        return invoices_get_all(conn)
+
+    if request.method == "POST":
+        new_invoice_num = request.form["invoice_num"]
+        new_invoice_series = request.form["invoice_series"]
+        new_invoice_type = request.form["invoice_type"]
+        new_invoice_client_id = request.form["invoice_client_id"]
+        sql = """INSERT INTO invoice
+                (invoice_num, invoice_series, invoice_type, client_id)
+                VALUES( ?, ?, ?, ?);"""
+        cursor = conn.execute(sql, (new_invoice_num, new_invoice_series, new_invoice_type, new_invoice_client_id))
+        conn.commit()
+        return f"Invoice created succesfully (invoiceid: {cursor.lastrowid})."
+
+
+@app.route("/invoices/<int:id>", methods=["GET", "DELETE"])
+def single_invoice_rd_operations():
+    conn, cursor = db_connection()
+    invoice = None
+    if request.method == "GET":
+        cursor.execute("SELECT * FROM invoice WHERE id=?", (id,))
+        rows = cursor.fetchall()
+        for r in rows:
+            invoice = r
+        if invoice is None:
+            return jsonify({'error': 'Resource not found'}), 404
+        return invoice
+
+    if request.method == "DELETE":
+        cursor.execute("DELETE FROM invoice WHERE id=?", (id,))
+        conn.commit()
+
+        if cursor.rowcount > 0:
+            return jsonify({'message': 'Operation was successful'}), 200
+        else:
+            return jsonify({'message': 'Operation was successful but no invoice to be deleted was found'}), 200
+
+
+def invoices_get_all(conn):
+    cursor = conn.execute("SELECT * FROM invoice")
+    return jsonify(cursor.fetchall())
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+'''
+@app.route('/')
+def index():
+    return 'Hello'
+'''
+
+'''
+@app.route('/<value>')
+def print_value(value):
+    return 'Routed value: {}'.format(value)
+'''
---
+++ db.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+conn = sqlite3.connect("invoices.sqlite")
+
+cursor = conn.cursor()
+sql_query = """ CREATE TABLE invoice (
+    id integer PRIMARY KEY,
+    invoice_num integer NOT NULL,
+    invoice_series text NOT NULL,
+    invoice_type TEXT CHECK (invoice_type IN ('O', 'C')) NOT NULL,
+    client_id integer NOT NULL
+)"""
+cursor.execute(sql_query)
